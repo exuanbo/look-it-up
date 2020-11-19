b997d08e00fba1d32539db3986ad35ee5b750408
exuanbo <exuanbo@protonmail.com>
2020-11-19 15:54:06 +0000

    refactor: extract `isStop()`, use ternary operator

--- src/matcher.ts
+++ src/matcher.ts
@@ -1,23 +1,25 @@
 import fs from 'fs'
 import path from 'path'
 
-export const stop = Symbol('lookUp.stop')
+export const stop = Symbol('stop')
+const isStop = (res: MatcherFnResult): res is symbol => res === stop
 
-type MatcherFnResult = string | undefined | typeof stop
+type MatcherFnResult = string | undefined | symbol
 type MatcherFn<S extends boolean> = (
   dir: string
-) => S extends true ? MatcherFnResult : MatcherFnResult | Promise<MatcherFnResult>
+) => MatcherFnResult | (S extends true ? never : Promise<MatcherFnResult>)
 export type Matcher<S extends boolean> = string | MatcherFn<S>
 
 interface MatchResult {
   dir?: string
   matched?: string
 }
-type Result<S extends boolean> = S extends true ? MatchResult : Promise<MatchResult>
+type Result<S extends boolean> = S extends true
+  ? MatchResult
+  : Promise<MatchResult>
 
-export const locate = (
-  path?: string
-): false | Pick<Required<MatchResult>, 'matched'> =>
+type LocateResult = false | MatchResult
+export const locate = (path?: string): LocateResult =>
   path !== undefined && fs.existsSync(path) && { matched: path }
 
 export const runMatcher = <S extends boolean>(
@@ -26,27 +28,22 @@ export const runMatcher = <S extends boolean>(
   sync: S
 ): Result<S> => {
   if (dir === path.dirname(dir)) {
-    if (sync) {
-      return ({} as unknown) as Result<S>
-    }
-    return Promise.resolve({}) as Result<S>
+    return ({} as unknown) as Result<S>
   }
 
   if (typeof matcher === 'string') {
     const matcherRes = path.join(dir, matcher)
     const match = locate(matcherRes)
-    if (match !== false) {
-      return match as Result<S>
-    }
-    return runMatcher(matcher, path.dirname(dir), sync)
+    return match === false
+      ? runMatcher(matcher, path.dirname(dir), sync)
+      : (match as Result<S>)
   }
 
   if (!sync) {
-    const matcherRes = matcher(dir) as Promise<MatcherFnResult>
-    return Promise.all([matcherRes])
+    return Promise.all([matcher(dir)])
       .then(res => res[0])
       .then(matcherRes => {
-        if (matcherRes === stop) {
+        if (isStop(matcherRes)) {
           return {}
         }
         const match = locate(matcherRes)
@@ -58,12 +55,11 @@ export const runMatcher = <S extends boolean>(
   }
 
   const matcherRes = matcher(dir) as MatcherFnResult
-  if (matcherRes === stop) {
+  if (isStop(matcherRes)) {
     return ({} as unknown) as Result<S>
   }
   const match = locate(matcherRes)
-  if (match !== false) {
-    return match as Result<S>
-  }
-  return runMatcher(matcher, path.dirname(dir), sync)
+  return match === false
+    ? runMatcher(matcher, path.dirname(dir), sync)
+    : (match as Result<S>)
 }
